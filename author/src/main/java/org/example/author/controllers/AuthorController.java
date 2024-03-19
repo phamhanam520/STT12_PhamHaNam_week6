82e6614b2ffb66074dbe82a10f37fdc702bbf5c3
Phạm Hà Nam <pnam55588@gmail.com>
2024-03-19 20:38:04 +0700

    fix client

--- author/src/main/java/org/example/author/controllers/AuthorController.java
+++ author/src/main/java/org/example/author/controllers/AuthorController.java
@@ -6,20 +6,27 @@ import org.example.author.repositories.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/author")
 @AllArgsConstructor
 public class AuthorController {
     @Autowired
     AuthorRepository authorRepository;
 
+    @GetMapping("/test")
+    String test(){
+        return "test";
+    }
+
     @PostMapping("/")
     Author post(){
         Author b = new Author();
-        b.setName("test");
+        b.setName("post new author");
         Author save = authorRepository.save(b);
         return save;
     }
